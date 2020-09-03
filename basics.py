526370cdd3eb397f56c6b1449a1721bb7cbde861
unknown <ajayr1174@gmail.com>
2020-09-03 19:45:40 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,17 @@
+var1 = 32
+var2 = 'ajay singh'
+print(str(var1) + var2)
+print(type(var1))
+# to print single quote we use double quotes outside and vise versa.
+
+print("ajay's laptop")
+print('"ajay Rawat"')
+
+# if we have to use both single and double quotes then we use \
+
+print('ajay\'s "laptop"')
+
+# raw string
+
+print(r"c:\n")
+# here interpreter neglect hte meaning of \n it just print the whole string as it is.
